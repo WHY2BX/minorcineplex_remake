9bd862e8cfe07297ff3f8f8aac87be42d8854a9b
Supitcha Wisescharoen <whyso2bx@gmail.com>
2023-09-22 18:20:37 +0700

    template

---
+++ connect.php
@@ -0,0 +1,16 @@
+<?php
+
+$servername = "localhost";
+$username = "admin";
+$password = "admin";
+$dbname = "minorcineplex";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+mysqli_set_charset($conn, "utf8");
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+?>
