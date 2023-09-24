e26f748cd10e8382197af24068c8a3aadb0be0b5
Supitcha Wisescharoen <whyso2bx@gmail.com>
2023-09-24 16:01:16 +0700

    edit booking page -->add header, sidebar + css

--- booking.php
+++ booking.php
@@ -4,18 +4,57 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Booking</title>
-  <link rel="stylesheet" href="booking_style.css">
+  <!-- Favicons -->
+  <link href="assets/img/favicon.png" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.gstatic.com" rel="preconnect">
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/quill/quill.snow.css" rel="stylesheet">
+  <link href="assets/vendor/quill/quill.bubble.css" rel="stylesheet">
+  <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+  <link href="assets/vendor/simple-datatables/style.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+
+
 </head>
 <body>
   
 
+<?php 
+
+      if (isset($_SESSION['first_name']) && ! empty($_SESSION['first_name'])){
+          include "header.php";  }
+      else
+          include "Guest_header.php";
+        
+  ?>
 
 
+  <?php 
+    if (isset($_SESSION['first_name']) && ! empty($_SESSION['first_name'])){
+        include "sidebar.php";   }
+    else
+        include "Guest_sidebar.php";
+  ?>
+
+  
+    <br><br><br><br><br><br><br><br><br><br><br>
     <!-- booking seat area -->
     <div class="movie-container">
       <label>Pick a time: </label>
       <select id="movie">
         <!-- รอ import รอบหนังจาก sql ถ้าเป็นไปได้ -->
+        <?php 
+        ?>
         <option value="250">Interstellar (Rs. 250)</option>
         <option value="200">Kabir Singh (Rs. 200)</option>
         <option value="150">Duniyadari (Rs. 150)</option>
