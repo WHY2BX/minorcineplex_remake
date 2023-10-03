7cc2098a8fce0c614f24b83b94e050c98534ce8c
unknown <65070233@kmitl.com>
2023-10-03 21:15:15 +0700

    new Booking

---
+++ bookingnew.php
@@ -0,0 +1,122 @@
+<?php 
+	//@session_start();
+	ob_start();
+	session_start();		
+	require_once('connect.php');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link href="bookingnew.css" rel="stylesheet">
+</head>
+<body>
+
+<div class="movie-container">
+  <label>Pick a movie: </label>
+  <form style = "background-color:white;" action="booking_action.php" method="post" novalidate>
+  <?php
+          //หนัง
+          $sql = "SELECT * FROM Movie";	
+          $result = mysqli_query($conn, $sql);
+              if (mysqli_num_rows($result) > 0) {                               
+                    echo "<select name ='Movie' class='dropdown-item'>";
+                  while($row = mysqli_fetch_assoc($result)) {	
+                        echo "<option>".$row["Movie_Name"]."</option>";                                          
+                        }
+                        echo "</select>";}
+  ?>
+</form>
+  <ul class="showcase">
+    <li>
+      <div class="seat"></div>
+      <small>N/A</small>
+    </li>
+    <li>
+      <div class="seat selected"></div>
+      <small>Selected</small>
+    </li>
+    <li>
+      <div class="seat occupied"></div>
+      <small>Occupied</small>
+    </li>    
+  </ul>
+  
+  <div class="container">
+    <div class="screen"></div>
+    
+    <div class="row">
+        <div class="seat"></div>
+        <div class="seat"></div>
+        <div class="seat"></div>
+        <div class="seat"></div>
+        <div class="seat"></div>
+        <div class="seat"></div>
+        <div class="seat"></div>
+        <div class="seat"></div>
+      </div>
+      <div class="row">
+        <div class="seat"></div>
+        <div class="seat"></div>
+        <div class="seat"></div>
+        <div class="seat "></div>
+        <div class="seat "></div>
+        <div class="seat"></div>
+        <div class="seat"></div>
+        <div class="seat"></div>
+      </div>
+      <div class="row">
+        <div class="seat"></div>
+        <div class="seat"></div>
+        <div class="seat"></div>
+        <div class="seat"></div>
+        <div class="seat"></div>
+        <div class="seat"></div>
+        <div class="seat "></div>
+        <div class="seat "></div>
+      </div>
+      <div class="row">
+        <div class="seat"></div>
+        <div class="seat"></div>
+        <div class="seat"></div>
+        <div class="seat"></div>
+        <div class="seat"></div>
+        <div class="seat"></div>
+        <div class="seat"></div>
+        <div class="seat"></div>
+      </div>
+      <div class="row">
+        <div class="seat"></div>
+        <div class="seat"></div>
+        <div class="seat"></div>
+        <div class="seat "></div>
+        <div class="seat "></div>
+        <div class="seat"></div>
+        <div class="seat"></div>
+        <div class="seat"></div>
+      </div>
+      <div class="row">
+        <div class="seat"></div>
+        <div class="seat"></div>
+        <div class="seat"></div>
+        <div class="seat"></div>
+        <div class="seat"></div>
+        <div class="seat"></div>
+        <div class="seat"></div>
+        <div class="seat"></div>
+      </div>
+    
+    <p class="text">
+      You have selected <span id="count">0</span> seats for the total price of Rs. <span id="total">0</span>
+    </p>
+  </div>
+</div>
+
+
+<script src="bookingnew.js"></script>
+</body>
+</html>
+
--- header.php
+++ header.php
@@ -84,6 +84,9 @@
           <hr class="dropdown-divider">
         </li>
 
+
+
+
         <li>
           <a class="dropdown-item d-flex align-items-center" href="logout.php">
             <i class="bi bi-box-arrow-right"></i>
