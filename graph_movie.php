7fc5e711a4ef4808cc57a5bc71c639a0ab0d5939
unknown <65070233@kmitl.com>
2023-10-14 21:00:51 +0700

    Graph

---
+++ graph_movie.php
@@ -0,0 +1,129 @@
+<?php 
+	//@session_start();
+	ob_start();
+	session_start();		
+	require_once('connect.php');
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <title>Movie Chart</title>
+
+
+    <style>
+    
+        body {
+            width: 550px;
+            margin: 3rem auto;
+        }
+
+        #chart-container {
+            width: 100%;
+            height: auto;
+        }
+
+    </style>
+
+<meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="assets/img/favicon.png" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.gstatic.com" rel="preconnect">
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/quill/quill.snow.css" rel="stylesheet">
+  <link href="assets/vendor/quill/quill.bubble.css" rel="stylesheet">
+  <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+  <link href="assets/vendor/simple-datatables/style.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Kanit&display=swap">
+
+
+</head>
+<body>
+
+<?php 
+
+        
+      if (isset($_SESSION['first_name']) && ! empty($_SESSION['first_name'])){
+          include "header.php";  }
+      else
+          include "Guest_header.php";
+        
+  ?>
+
+  <?php 
+      if(isset($_SESSION['name']) == 'Manager'){
+        include "Manager_sidebar.php";
+    }
+    else if (isset($_SESSION['first_name']) && ! empty($_SESSION['first_name'])){
+        include "sidebar.php";   }
+    else
+        include "Guest_sidebar.php";
+  ?>
+
+
+
+
+    <div id="chart-container">
+        <canvas id="MovieCanvas"></canvas>
+    </div>
+
+
+    <script src="https://code.jquery.com/jquery-3.5.0.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/chart.js@2.8.0"></script>
+    <script>
+        $(document).ready(function() {
+            showGraph();
+        });
+
+        function showGraph(){
+            {
+                $.post("graph_data_Movie.php", function(data) {
+                    console.log(data);
+                    let movie_name = [];
+                    let earning = [];
+
+                    for (let i in data) {
+                        movie_name.push(data[i].Movie_Name);
+                        earning.push(data[i].earnings);
+                    }
+
+                    let chartdata = {
+                        labels: movie_name,
+                        datasets: [{
+                                label: 'Movie Total earnings',
+                                backgroundColor: '#49e2ff',
+                                borderColor: '#46d5f1',
+                                hoverBackgroundColor: '#CCCCCC',
+                                hoverBorderColor: '#666666',
+                                data: earning
+                        }]
+                    };
+
+                    let graphTarget = $('#MovieCanvas');
+                    let barGraph = new Chart(graphTarget, {
+                        type: 'bar',
+                        data: chartdata
+                    })
+                })
+            }
+        }
+    </script>
+
+</body>
+</html>
