773c5d952cc7800c2a0163ad4d4399f48a3eef7e
Supitcha Wisescharoen <whyso2bx@gmail.com>
2023-10-15 21:50:09 +0700

    test

--- register_action.php
+++ register_action.php
@@ -1,27 +1,27 @@
 <?php
-	session_cache_limiter('private, must-revalidate');
-	session_cache_expire(60);
-	ob_start();
-	session_start();
-	$_SESSION = array();
-	session_destroy(); 
+    session_cache_limiter('private, must-revalidate');
+    session_cache_expire(60);
+    ob_start();
+    session_start();
+    $_SESSION = array();
+    session_destroy(); 
 ?>
 <?php require_once('connect.php'); ?>
 <?php
   $firstname = $_POST['firstname'];
   $lastname = $_POST['lastname'];
-	$username = $_POST['username'];
-	$password = $_POST['password'];
-	
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    
 
 
 ?>
 
 <?php
-	
+    
     try{
-		$sql = "INSERT INTO Member (First_Name,Last_Name,Username,Password) VALUES ('$firstname', '$lastname', '$username', '$password')";  
-		$result = mysqli_query($conn, $sql);
+        $sql = "INSERT INTO Member (First_Name,Last_Name,Username,Password) VALUES ('$firstname', '$lastname', '$username', '$password')";  
+        $result = mysqli_query($conn, $sql);
 
         if ($conn->query($sql) === TRUE) {
             echo "New record created successfully";
@@ -35,6 +35,6 @@
         echo "<script> window.open('login.php','_self'); </script>";
     }
          
-			
+            
           mysqli_close($conn);
 ?>
